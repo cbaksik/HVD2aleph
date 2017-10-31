0eb4b81e0480b2232090838ef048aa555f86f9f0
sam san <khmereshop@yahoo.com>
2017-10-31 14:01:26 -0400

    make facet wider

--- js/custom.js
+++ js/custom.js
@@ -2295,12 +2295,17 @@ angular.module('viewCustom').component('prmSearchResultAvailabilityLineAfter', {
  * Created by samsan on 9/25/17.
  */
 
-angular.module('viewCustom').controller('prmSearchResultListAfterCtrl', ['customGoogleAnalytic', function (customGoogleAnalytic) {
+angular.module('viewCustom').controller('prmSearchResultListAfterCtrl', ['customGoogleAnalytic', '$element', function (customGoogleAnalytic, $element) {
     var vm = this;
     var cga = customGoogleAnalytic;
     //capture search result and report to google analytic
     vm.$onChanges = function () {
         cga.setPage('/search', vm.parentCtrl.query);
+        if (vm.parentCtrl.isFavorites === false) {
+            // remove left margin on result list grid
+            var el = $element[0].parentNode.parentNode.parentNode;
+            el.children[0].remove();
+        }
     };
 }]);
 
--- js/prm-search-result-list-after.js
+++ js/prm-search-result-list-after.js
@@ -5,12 +5,17 @@
 
 
 angular.module('viewCustom')
-    .controller('prmSearchResultListAfterCtrl',['customGoogleAnalytic',function (customGoogleAnalytic) {
+    .controller('prmSearchResultListAfterCtrl',['customGoogleAnalytic','$element',function (customGoogleAnalytic, $element) {
         var vm=this;
         var cga=customGoogleAnalytic;
         //capture search result and report to google analytic
         vm.$onChanges=function () {
             cga.setPage('/search', vm.parentCtrl.query);
+            if(vm.parentCtrl.isFavorites===false) {
+                // remove left margin on result list grid
+                var el = $element[0].parentNode.parentNode.parentNode;
+                el.children[0].remove();
+            }
         };
 
 
