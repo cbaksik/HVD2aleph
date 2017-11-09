875e6c9a8c584ffd272891f505d06db424bf8e24
sam san <khmereshop@yahoo.com>
2017-11-09 11:10:56 -0500

    add css breadcrumbs icon lock

--- js/custom.js
+++ js/custom.js
@@ -2026,33 +2026,6 @@ angular.module('viewCustom').component('prmAuthenticationAfter', {
 });
 
 /**
- * Created by samsan on 11/6/17.
- */
-
-angular.module('viewCustom').controller('prmBreadcrumbsAfterCtrl', ['$element', function ($element) {
-    var vm = this;
-    vm.$onChanges = function () {
-        //console.log('**** prm-breadcrumbs-after ****');
-        //console.log(vm.parentCtrl);
-    };
-
-    vm.hover = function (facet) {
-        if (facet.hover) {
-            facet.hover = false;
-        } else {
-            facet.hover = true;
-        }
-    };
-}]);
-
-angular.module('viewCustom').component('prmBreadcrumbsAfter', {
-    bindings: { parentCtrl: '<' },
-    controller: 'prmBreadcrumbsAfterCtrl',
-    controllerAs: 'vm'
-    //templateUrl:'/primo-explore/custom/HVD2/html/prm-breadcrumbs-after.html'
-});
-
-/**
  * Created by samsan on 8/10/17.
  * This component add a "Finding Aid" button and make a link
  */
